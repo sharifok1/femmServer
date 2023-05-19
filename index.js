12e5c76fbdf8c09cbba10befa7a986e768ba7a17
sharifok1 <sharifok1@gmail.com>
2023-05-19 21:18:28 +0600

    added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT||5000
 
 app.use(cors());
 app.use(express.json());
-// app.use(fileupload());
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@innerfashion.rsckbv2.mongodb.net/?retryWrites=true&w=majority`
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@innerfashion.rsckbv2.mongodb.net/?retryWrites=true&w=majority`
