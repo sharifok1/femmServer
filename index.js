f39f8269b839858674b908a1d0bf8992f0b2a541
sharifok1 <sharifok1@gmail.com>
2023-06-09 10:23:02 +0600

    add 5000

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config();
 const  ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT||8000
+const port = process.env.PORT||5000
 
 app.use(cors());
 app.use(express.json());
